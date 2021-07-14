ae5f19feec0ae9746980e24ad626ceefe6a98c67
Jesus Urquidez Calvo <jesus123076@gmail.com>
2021-07-14 02:03:43 -0700

    BottomBar and some fragments

--- app/src/main/java/com/example/uninote/MainActivity.java
+++ app/src/main/java/com/example/uninote/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.uninote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_todo:
+                        //fragment = new PostsFragment();
+                        break;
+                    case R.id.action_reminder:
+                        //fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_share:
+                        //fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_profile:
+                        //fragment = new ProfileFragment();
+                        break;
+                    default:
+                        //fragment = new ComposeFragment();
+                        break;
+                }
+                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_reminder);
     }
 }
